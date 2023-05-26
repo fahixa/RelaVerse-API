bb9184448adf0f596beaca5b0cb1bab3b2829a34
Fakhrity Hikmawan <fakhrityhikmawan@gmail.com>
2023-05-27 04:05:36 +0700

    Add API Documentation to the root path

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const path = require('path');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -8,5 +9,9 @@ app.use(bodyParser.json());
 app.use('/api/login', require('./routes/login'));
 app.use('/api/register', require('./routes/register'));
 app.use('/api/users', require('./routes/users'));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
 module.exports = app;
