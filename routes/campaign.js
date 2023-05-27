8022ca66b814fd4b143f1a850cf3cc27a83973ea
Fakhrity Hikmawan <fakhrityhikmawan@gmail.com>
2023-05-28 04:14:09 +0700

    Add Cloud Storage Bucket

--- routes/campaign.js
+++ routes/campaign.js
@@ -1,28 +1,20 @@
-express = require('express');
+const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const Joi = require('joi');
 const pool = require('../config/db');
-const fs = require('fs');
 const verifyToken = require('../middleware/verifyToken');
+const { Storage } = require('@google-cloud/storage');
+const path = require('path');
 
-const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, './uploads/');
-  },
-  filename: function(req, file, cb) {
-    const now = new Date().toISOString().replace(/:/g, "-");
-    const filename = now + file.originalname;
-
-    // fs.access('./uploads/' + filename, fs.constants.F_OK, (err) => {
-    //   if(err) {
-    //     cb(null, filename);
-    //   } else {
-    //     cb(new Error('File with this name already exists'));
-    //   }
-    // });
-  }
+const storage = new Storage({
+  keyFilename: path.join(__dirname, "../keys/bangkit-386813-0aa8ddedf7ae.json"),
+  projectId: "bangkit-386813",
 });
+
+const bucket = storage.bucket("relaverse");
+
+const multerStorage = multer.memoryStorage();
 
 const fileFilter = (req, file, cb) => {
   if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
@@ -33,7 +25,7 @@ const fileFilter = (req, file, cb) => {
 };
 
 const upload = multer({
-  storage: storage,
+  storage: multerStorage,
   fileFilter: fileFilter
 });
 
@@ -48,104 +40,94 @@ const campaignSchema = Joi.object({
   date: Joi.date().required()
 });
 
-router.post('/', upload.single('photoEvent'),verifyToken, (req, res) => {
-  
-  if (req.fileValidationError) {
-    return res.status(400).json({ error: true, message: req.fileValidationError });
-  }
+router.post('/', upload.single('photoEvent'),verifyToken, async (req, res, next) => {
+  try {
+    const { title, name, userId, latitude, longitude, contact, description, date } = req.body;
+    
+    const { error } = campaignSchema.validate({ title, name, userId, latitude, longitude, contact, description, date });
+    if (error) {
+      return res.status(400).json({ error: true, message: error.details[0].message });
+    }
 
-  const { title, name, userId, latitude, longitude, contact, description, date } = req.body;
-  
-  const { error } = campaignSchema.validate({ title, name, userId, latitude, longitude, contact, description, date });
-  if (error) {
-    return res.status(400).json({ error: true, message: error.details[0].message });
-  }
+    if (!req.file) {
+      return res.status(400).json({ error: true, message: 'photoEvent is required' });
+    }
 
-  pool.getConnection((err, connection) => {
-    if (err) {
-      res.status(500).json({ error: true, message: 'Failed to connect to database' });
-    } else {
-      const query = 'SELECT * FROM campaigns WHERE title = ?';
-      connection.query(query, [title], (err, results) => {
+    if (req.file.size > 1024 * 1024 * 5) {
+      return res.status(400).json({ error: true, message: 'File size is too large. Maximum size is 5MB.' });
+    }
+
+    const newFileName = Date.now() + "-" + req.file.originalname;
+    const fileUpload = bucket.file(newFileName);
+
+    const blobStream = fileUpload.createWriteStream({
+      metadata: {
+        contentType: req.file.mimetype
+      }
+    });
+
+    blobStream.on("error", (error) => {
+      console.error('Something went wrong!', error);
+      return res.status(500).json({ error: true, message: "Something went wrong uploading your file" });
+    });
+
+    blobStream.on("finish", () => {
+      const publicUrl = `https://storage.googleapis.com/${bucket.name}/${fileUpload.name}`;
+      createCampaign(publicUrl);
+    });
+
+    blobStream.end(req.file.buffer);
+
+    function createCampaign(photoEvent) {
+      pool.getConnection((err, connection) => {
         if (err) {
-          connection.release();
-          res.status(500).json({ error: true, message: 'Failed to execute query', error_details: err });
-        } else if(results.length > 0) {
-          connection.release();
-          res.status(400).json({ error: true, message: 'Campaign title already exists. Please choose a different title.' });
+          console.error('Failed to connect to database', err);
+          return res.status(500).json({ error: true, message: 'Failed to connect to database' });
         } else {
-          const query = 'SELECT * FROM users WHERE id = ?';
-          connection.query(query, [userId], (err, results) => {
-            if (err) {
-              connection.release();
-              res.status(500).json({ error: true, message: 'Failed to execute query', error_details: err });
-            } else if (results.length === 0) {
-              connection.release();
-              res.status(400).json({ error: true, message: 'Invalid userId - Foreign key constraint failed' });
-            } else {
-              const user = results[0];
-          
-              if (user.id !== req.userId) {
+        const query = 'SELECT * FROM campaigns WHERE title = ?';
+        connection.query(query, [title], (err, results) => {
+          if (err) {
+            connection.release();
+            res.status(500).json({ error: true, message: 'Failed to execute query', error_details: err });
+          } else if(results.length > 0) {
+            connection.release();
+            res.status(400).json({ error: true, message: 'Campaign title already exists. Please choose a different title.' });
+          } else {
+            const query = 'SELECT * FROM users WHERE id = ?';
+            connection.query(query, [userId], (err, results) => {
+              if (err) {
                 connection.release();
-                res.status(401).json({ error: true, message: 'Unauthorized' });
+                res.status(500).json({ error: true, message: 'Failed to execute query', error_details: err });
+              } else if (results.length === 0) {
+                connection.release();
+                res.status(400).json({ error: true, message: 'Invalid userId - Foreign key constraint failed' });
               } else {
-                // pastikan req.file ada sebelum menyimpan path gambar ke database
-                const photoEvent = req.file ? req.file.path : null;
-                const insertQuery = 'INSERT INTO campaigns (title, name, userId, latitude, longitude, contact, description, date, photoEvent) VALUES (?,?,?,?,?,?,?,?,?)';
-                connection.query(insertQuery, [title, name, userId, latitude, longitude, contact, description, date, photoEvent], (err, results) => {
+                const user = results[0];
+            
+                if (user.id !== req.userId) {
                   connection.release();
-                  if(err) {
-                    res.status(500).json({ error: true, message: 'Failed to executequery', error_details: err });
-                  } else {
-                    res.status(201).json({ error: false, message: 'Campaign created successfully' });
-                  }
-                });
+                  res.status(401).json({ error: true, message: 'Unauthorized' });
+                } else {
+                  const insertQuery = 'INSERT INTO campaigns (title, name, userId, latitude, longitude, contact, description, date, photoEvent) VALUES (?,?,?,?,?,?,?,?,?)';
+                  connection.query(insertQuery, [title, name, userId, latitude, longitude, contact, description, date, photoEvent], (err, results) => {
+                    connection.release();
+                    if(err) {
+                      res.status(500).json({ error: true, message: 'Failed to executequery', error_details: err });
+                    } else {
+                      res.status(201).json({ error: false, message: 'Campaign created successfully' });
+                    }
+                  });
+                }
               }
-            }
-          });
-        }
-      });
-    }
-  });
-});
-
-router.get('/', verifyToken, (req, res) => {
-  pool.getConnection((err,connection) => {
-    if (err) {
-      res.status(500).json({ error: true, message: 'Failed to connect to database' });
-    } else {
-      const query = 'SELECT * FROM campaigns WHERE userId = ?';
-      connection.query(query, [req.userId], (err, results) => {
-        if (err) {
-          connection.release();
-          res.status(500).json({ error: true,message: 'Failed to execute query' });
-        } else {
-          connection.release();
-          res.status(200).json({ error: false, campaigns: results });
-        }
-      });
-    }
-  });
-});
-
-router.get('/all', (req, res) => {
-  pool.getConnection((err, connection) => {
-    if (err) {
-      res.status(500).json({ error: true, message: 'Failed to connect to database' });
-    } else {
-      const query = 'SELECT * FROM campaigns';
-
-      connection.query(query, (err, results) => {
-        connection.release();
-
-        if (err) {
-          res.status(500).json({ error: true, message: 'Failed to execute query', error_details: err });
-        } else {
-          res.status(200).json({ error: false, campaigns: results });
-        }
-      });
-    }
-  });
+            });
+          }
+        });
+      }
+    });
+  } }catch (err) {
+    console.error('An error occurred', err);
+    return res.status(500).json({ error: true, message: 'An error occurred while processing your request' });
+  }
 });
 
 module.exports = router;
