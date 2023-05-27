d71c538c90d3baa73a934adc2a2a745cc8d26100
M. Aimar Rizki Utama <108123119+aimrzky@users.noreply.github.com>
2023-05-27 21:25:54 +0700

    Bugs Fix Campaign

--- routes/campaign.js
+++ routes/campaign.js
@@ -1,4 +1,4 @@
-const express = require('express');
+express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const Joi = require('joi');
@@ -14,7 +14,7 @@ const storage = multer.diskStorage({
   }
 });
 
-const fileFilter =(req, file, cb) => {
+const fileFilter = (req, file, cb) => {
   if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
     cb(null, true);
   } else {
@@ -39,68 +39,78 @@ const campaignSchema = Joi.object({
 });
 
 router.post('/', upload.single('photoEvent'), verifyToken, (req, res) => {
-    const { title, name, userId, latitude, longitude, contact, description, date } = req.body;
-    
-    const { error } = campaignSchema.validate({ title, name, userId, latitude, longitude, contact, description, date });
-    if (error) {
+  const { title, name, userId, latitude, longitude, contact, description, date } = req.body;
+  
+  const { error } = campaignSchema.validate({ title, name, userId, latitude, longitude, contact, description, date });
+  if (error) {
     return res.status(400).json({ error: true, message: error.details[0].message });
-    }
+  }
 
-    pool.getConnection((err, connection) => {
+  pool.getConnection((err, connection) => {
     if (err) {
-        res.status(500).json({ error: true, message: 'Failed to connect to database' });
+      res.status(500).json({ error: true, message: 'Failed to connect to database' });
     } else {
-        const query = 'SELECT * FROM users WHERE id = ?';
-        connection.query(query, [userId], (err, results) => {
+      const query = 'SELECT * FROM campaigns WHERE title = ?';
+      connection.query(query, [title], (err, results) => {
         if (err) {
-            connection.release();
-            res.status(500).json({ error: true, message: 'Failed to execute query', error_details: err });
-        } else if (results.length === 0) {
-            connection.release();
-            res.status(400).json({ error: true, message: 'Invalid userId - Foreign key constraint failed' });
+          connection.release();
+          res.status(500).json({ error: true, message: 'Failed to execute query', error_details: err });
+        } else if(results.length > 0) {
+          connection.release();
+          res.status(400).json({ error: true, message: 'Campaign title already exists. Please choose a different title.' });
         } else {
-            const user = results[0];
-        
-            if (user.id !== req.userId) {
-            connection.release();
-            res.status(401).json({ error: true, message: 'Unauthorized' });
+          const query = 'SELECT * FROM users WHERE id = ?';
+          connection.query(query, [userId], (err, results) => {
+            if (err) {
+              connection.release();
+              res.status(500).json({ error: true, message: 'Failed to execute query', error_details: err });
+            } else if (results.length === 0) {
+              connection.release();
+              res.status(400).json({ error: true, message: 'Invalid userId - Foreign key constraint failed' });
             } else {
-            // pastikan req.file ada sebelum menyimpan path gambar ke database
-            const photoEvent = req.file ? req.file.path : null;
-            const insertQuery = 'INSERT INTO campaigns (title, name, userId, latitude, longitude, contact, description, date, photoEvent) VALUES (?,?,?,?,?,?,?,?,?)';
-            connection.query(insertQuery, [title, name, userId, latitude, longitude, contact, description, date, photoEvent], (err, results) => {
+              const user = results[0];
+          
+              if (user.id !== req.userId) {
                 connection.release();
-                if (err) {
-                res.status(500).json({ error: true, message: 'Failed to executequery', error_details: err });
-                } else {
-                res.status(201).json({ error: false, message: 'Campaign created successfully' });
-                }
-            });
+                res.status(401).json({ error: true, message: 'Unauthorized' });
+              } else {
+                // pastikan req.file ada sebelum menyimpan path gambar ke database
+                const photoEvent = req.file ? req.file.path : null;
+                const insertQuery = 'INSERT INTO campaigns (title, name, userId, latitude, longitude, contact, description, date, photoEvent) VALUES (?,?,?,?,?,?,?,?,?)';
+                connection.query(insertQuery, [title, name, userId, latitude, longitude, contact, description, date, photoEvent], (err, results) => {
+                  connection.release();
+                  if(err) {
+                    res.status(500).json({ error: true, message: 'Failed to executequery', error_details: err });
+                  } else {
+                    res.status(201).json({ error: false, message: 'Campaign created successfully' });
+                  }
+                });
+              }
             }
+          });
         }
-        });
+      });
     }
-    });
+  });
 });
 
-
 router.get('/', verifyToken, (req, res) => {
-    pool.getConnection((err, connection) => {
-      if (err) {
-        res.status(500).json({ error: true, message: 'Failed to connect to database' });
-      } else {
-        const query = 'SELECT * FROM campaigns WHERE userId = ?';
-        connection.query(query, [req.userId], (err, results) => {
-          if (err) {
-            connection.release();
-            res.status(500).json({ error: true,message: 'Failed to execute query' });
-          } else {
-            connection.release();
-            res.status(200).json({ error: false, campaigns: results });
-          }
-        });
-      }
-    });
+  pool.getConnection((err,connection) => {
+    if (err) {
+      res.status(500).json({ error: true, message: 'Failed to connect to database' });
+    } else {
+      const query = 'SELECT * FROM campaigns WHERE userId = ?';
+      connection.query(query, [req.userId], (err, results) => {
+        if (err) {
+          connection.release();
+          res.status(500).json({ error: true,message: 'Failed to execute query' });
+        } else {
+          connection.release();
+          res.status(200).json({ error: false, campaigns: results });
+        }
+      });
+    }
+  });
 });
 
 module.exports = router;
