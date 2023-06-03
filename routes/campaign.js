64415b8efa48f2ad55cdae2a93ab997e032d1ef5
Fakhrity Hikmawan <fakhrityhikmawan@gmail.com>
2023-06-03 16:54:30 +0700

    Update API Documentation & Remove API /joined/:campaignId/get

--- routes/campaign.js
+++ routes/campaign.js
@@ -282,50 +282,6 @@ router.get('/joined/:campaignId', verifyToken, (req, res) => {
   });
 });
 
-router.get('/joined/:campaignId/get', verifyToken, (req, res) => {
-  const campaignId = req.params.campaignId;
-  const userId = req.userId;
-
-  // Check if the campaign exists
-  pool.query('SELECT * FROM campaigns WHERE id = ?', [campaignId], (err, campaignResults) => {
-    if (err) {
-      return res.status(500).json({ error: true, message: 'Failed to execute query' });
-    }
-    if (campaignResults.length === 0) {
-      return res.status(404).json({ error: true, message: 'Campaign not found' });
-    }
-
-    // Get the campaign details
-    const campaign = campaignResults[0];
-    const { id, latitude, longitude } = campaign;
-
-    // Check if the user is a participant
-    pool.query(
-      'SELECT users.id AS userId, users.name FROM campaign_participants JOIN users ON campaign_participants.user_id = users.id WHERE campaign_participants.campaign_id = ?',
-      [campaignId],
-      (err, participantResults) => {
-        if (err) {
-          return res.status(500).json({ error: true, message: 'Failed to execute query' });
-        }
-
-        const userList = participantResults.map((participant) => ({
-          userId: participant.userId,
-          name: participant.name,
-        }));
-
-        return res.status(200).json({
-          error: false,
-          message: 'Campaign details fetched successfully',
-          campaignId: id,
-          latitude,
-          longitude,
-          userList,
-        });
-      }
-    );
-  });
-});
-
 router.get('/volunteer/:userId', verifyToken, (req, res) => {
   const userId = req.params.userId;
 
