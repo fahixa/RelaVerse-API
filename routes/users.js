d5bed1a1d68acca56ed117956fca5830a0eee795
M. Aimar Rizki Utama <108123119+aimrzky@users.noreply.github.com>
2023-06-10 01:58:46 +0700

    Update Campaign & Users
    All Bugs Fixed
    + Add features to leave campaign
    + Add features to delete campaign
    + Add features now user can edit their profile

--- routes/users.js
+++ routes/users.js
@@ -130,4 +130,70 @@ router.get('/:id', verifyToken, (req, res) => {
   });
 });
 
+router.put('/edit-profile/:id', verifyToken, (req, res) => {
+  const userId = req.params.id;
+  const { name, email, phoneNumber } = req.body;
+
+  pool.getConnection((err, connection) => {
+    if (err) {
+      res.status(500).json({ error: true, message: 'Failed to connect to database' });
+    } else {
+      const query = 'SELECT * FROM users WHERE id = ?';
+      connection.query(query, [userId], (err, results) => {
+        if (err) {
+          connection.release();
+          res.status(500).json({ error: true, message: 'Failed to execute query' });
+        } else if (results.length === 0) {
+          connection.release();
+          res.status(404).json({ error: true, message: 'User not found' });
+        } else {
+          const user = results[0];
+
+          if (user.id !== req.userId) {
+            connection.release();
+            res.status(401).json({ error: true, message: 'Unauthorized' });
+          } else {
+            let updateQuery = 'UPDATE users SET';
+
+            const updateData = [];
+            const updateValues = [];
+
+            if (name) {
+              updateData.push('name = ?');
+              updateValues.push(name);
+            }
+
+            if (email) {
+              updateData.push('email = ?');
+              updateValues.push(email);
+            }
+
+            if (phoneNumber) {
+              updateData.push('phone_number = ?');
+              updateValues.push(phoneNumber);
+            }
+
+            if (updateData.length === 0) {
+              connection.release();
+              res.status(400).json({ error: true, message: 'No data to update' });
+            } else {
+              updateQuery += ` ${updateData.join(', ')} WHERE id = ?`;
+              updateValues.push(userId);
+
+              connection.query(updateQuery, updateValues, (err, results) => {
+                connection.release();
+                if (err) {
+                  res.status(500).json({ error: true, message: 'Failed to execute query' });
+                } else {
+                  res.status(200).json({ error: false, message: 'Profile updated successfully' });
+                }
+              });
+            }
+          }
+        }
+      });
+    }
+  });
+});
+
 module.exports = router;
